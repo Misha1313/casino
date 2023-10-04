5a6aab66efdf5f33e2c15e17321b27c937646dd0
Mikheil Marghishvili <m.marghishvili@adjarabet.com>
2023-10-04 18:46:14 +0400

    v1

---
+++ docs/readme.md
@@ -0,0 +1,9 @@
+# NestJS Boilerplate Documentation
+
+---
+
+## Table of Contents
+
+- [Installing and Running](installing-and-running.md)
+- [Working with database](database.md)
+- [Serialization](serialization.md)
---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import databaseConfig from './config/database.config';
+import appConfig from './config/app.config';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './database/typeorm-config.service';
+import { HomeModule } from './home/home.module';
+import { DataSource, DataSourceOptions } from 'typeorm';
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, appConfig],
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+      dataSourceFactory: async (options: DataSourceOptions) => {
+        return new DataSource(options).initialize();
+      },
+    }),
+    UsersModule,
+    HomeModule,
+  ],
+})
+export class AppModule {}
